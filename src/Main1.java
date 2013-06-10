38f2a59585ec63d2a782f0650a5e7a3f10cb119d
Jan Vincent Liwanag <jvliwanag@gmail.com>
2013-06-10 16:49:01 +0800

    Add property based config

--- src/Main1.java
+++ src/Main1.java
@@ -1,11 +1,20 @@
 import java.net.InetAddress;
 import java.net.URI;
-import java.util.ArrayList;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
 
+import net.sourceforge.peers.Config;
 import net.sourceforge.peers.JavaConfig;
 import net.sourceforge.peers.Logger;
 import net.sourceforge.peers.media.MediaMode;
+import net.sourceforge.peers.sip.syntaxencoding.SipURI;
+import net.sourceforge.peers.sip.syntaxencoding.SipUriSyntaxException;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+import com.ezuce.oacdlt.AgentConnectionFactory;
 import com.ezuce.oacdlt.AgentManager;
 import com.ezuce.oacdlt.AgentWebConnectionFactory;
 import com.ezuce.oacdlt.CallerManager;
@@ -15,85 +24,143 @@ import com.ezuce.oacdlt.SupervisorManager;
 public class Main1 {
 	public static void main(String... args) throws Exception {
 
-		URI loginURI = URI.create("http://oacddev.ezuce.com:8936/login");
-		URI conURI = URI.create("ws://oacddev.ezuce.com:8936/wsock");
+		Configuration config = new PropertiesConfiguration("config.properties");
 
 		Logger logger = new Logger(null);
 
-		String password = "password";
-		String sipPassword = "1234";
-		String sipDomain = "oacddev.ezuce.com";
-
-		// caller settings
-		int callerFrom = 1002;
-		int callerTo = 1002;
-
-		int idleMinMs = 2000;
-		int idleMaxMs = 5000;
-		int callMinMs = 5000;
-		int callMaxMs = 8000;
-
-		ArrayList<String> lines = new ArrayList<>();
-		lines.add("90");
-
-		String inetAddress = "10.24.7.1";
-
-		// agent settings
-		int agentFrom = 1005;
-		int agentTo = 1009;
-
-		int ringMinMs = 1000;
-		int ringMaxMs = 5000;
-		int agentCallMinMs = 2000;
-		int agentCallMaxMs = 10000;
-
-		// supervisor settings
-		int supervisorFrom = 1003;
-		int supervisorTo = 1004;
-
-		// sip config
-
-		JavaConfig baseConfig = new JavaConfig();
-		baseConfig.setLocalInetAddress(InetAddress.getByName(inetAddress));
-		// baseConfig.setLocalInetAddress(InetAddress.getLocalHost())
-		baseConfig.setDomain(sipDomain);
-		baseConfig.setMediaMode(MediaMode.none);
+		// sip
+		Config baseConfig = getSipConfig(config);
 
 		// phone factory
 		PhoneFactory phoneF = new PhoneFactory(baseConfig, logger);
-		//
-		// // caller manager
-		CallerManager callerM = new CallerManager(callerFrom, callerTo,
-				sipPassword, idleMinMs, idleMaxMs, callMinMs, callMaxMs, lines,
-				phoneF);
 
-		callerM.register();
-		callerM.startCalls();
+		// caller settings
+		CallerManager callerM = getCallerManager(config, phoneF);
 
-		// conn factory
-		AgentWebConnectionFactory connF = new AgentWebConnectionFactory(
-				loginURI, conURI);
+		// agent settings
+		AgentConnectionFactory connF = getConnectionFactory(config);
 
-		// agent Manager
-		AgentManager agentM = new AgentManager(agentFrom, agentTo, password,
-				sipPassword, ringMinMs, ringMaxMs, agentCallMinMs,
-				agentCallMaxMs, connF, phoneF);
-
-		// supervisor Manager
-		SupervisorManager supervisorM = new SupervisorManager(supervisorFrom,
-				supervisorTo, password, sipPassword, connF, phoneF);
+		AgentManager agentM = getAgentManager(config, phoneF, connF);
+		SupervisorManager supervisorM = getSupervisorManager(config, phoneF,
+				connF);
 
 		callerM.register();
 		agentM.start();
 		supervisorM.start();
 
+		Thread.sleep(3000);
 		callerM.startCalls();
+	}
 
-		// Phone p = phoneF.createPhone("1095", "1234", new
-		// DummyPhoneListener());
-		// p.register();
-		// p.reset();
-		// Thread.sleep(5000);
-		// System.out.println("ok");
+	private static SupervisorManager getSupervisorManager(Configuration config,
+			PhoneFactory phoneF, AgentConnectionFactory connF) {
+		// supervisor settings
+		Configuration supervisorConf = config.subset("supervisor");
+
+		int supervisorFrom = supervisorConf.getInt("ext.start");
+		int supervisorTo = supervisorConf.getInt("ext.end");
+		String supervisorPassword = supervisorConf.getString("password");
+		String supervisorSipPassword = supervisorConf.getString("sip_password");
+
+		// supervisor Manager
+		SupervisorManager supervisorM = new SupervisorManager(supervisorFrom,
+				supervisorTo, supervisorPassword, supervisorSipPassword, connF,
+				phoneF);
+		return supervisorM;
+	}
+
+	private static AgentManager getAgentManager(Configuration config,
+			PhoneFactory phoneF, AgentConnectionFactory connF) {
+		Configuration agentConf = config.subset("agent");
+
+		int agentFrom = agentConf.getInt("ext.start");
+		int agentTo = agentConf.getInt("ext.end");
+		String agentPassword = agentConf.getString("password");
+		String agentSipPassword = agentConf.getString("sip_password");
+
+		int ringMinMs = agentConf.getInt("ans.min");
+		int ringMaxMs = agentConf.getInt("ans.max");
+		int agentCallMinMs = agentConf.getInt("call.min");
+		int agentCallMaxMs = agentConf.getInt("call.max");
+
+		// agent Manager
+		AgentManager agentM = new AgentManager(agentFrom, agentTo,
+				agentPassword, agentSipPassword, ringMinMs, ringMaxMs,
+				agentCallMinMs, agentCallMaxMs, connF, phoneF);
+		return agentM;
+	}
+
+	private static AgentConnectionFactory getConnectionFactory(
+			Configuration config) {
+		URI loginURI = URI.create(config.getString("web.login_uri"));
+		URI conURI = URI.create(config.getString("web.wsock_uri"));
+
+		// conn factory
+		AgentWebConnectionFactory connF = new AgentWebConnectionFactory(
+				loginURI, conURI);
+		return connF;
+	}
+
+	private static CallerManager getCallerManager(Configuration config,
+			PhoneFactory phoneF) {
+		Configuration callerConf = config.subset("caller");
+		int callerFrom = callerConf.getInt("ext.start");
+		int callerTo = callerConf.getInt("ext.end");
+		String callerSipPassword = callerConf.getString("sip_password");
+
+		int idleMinMs = callerConf.getInt("idle.min");
+		int idleMaxMs = callerConf.getInt("idle.max");
+		int callMinMs = callerConf.getInt("call.min");
+		int callMaxMs = callerConf.getInt("call.max");
+
+		List<String> lines = Arrays.asList(config.getStringArray("lines"));
+		// caller manager
+		CallerManager callerM = new CallerManager(callerFrom, callerTo,
+				callerSipPassword, idleMinMs, idleMaxMs, callMinMs, callMaxMs,
+				lines, phoneF);
+		return callerM;
+	}
+
+	private static Config getSipConfig(Configuration config)
+			throws SipUriSyntaxException, UnknownHostException {
+		Configuration sipConf = config.subset("sip");
+		String sipDomain = sipConf.getString("domain");
+		String sipProxy = sipConf.getString("outbound_proxy");
+		String sipLocalInetAddress = sipConf.getString("local_inet_address");
+		String sipPublicInetAddress = sipConf.getString("public_inet_address");
+		String sipPlaybackFile = sipConf.getString("playbackFile");
+
+		JavaConfig baseConfig = new JavaConfig();
+		baseConfig.setDomain(sipDomain);
+
+		if (!isEmpty(sipProxy)) {
+			SipURI r = new SipURI(sipProxy.startsWith("sip:") ? sipProxy
+					: "sip:" + sipProxy);
+			baseConfig.setOutboundProxy(r);
+		}
+
+		if (!isEmpty(sipLocalInetAddress)) {
+			baseConfig.setLocalInetAddress(InetAddress
+					.getByName(sipLocalInetAddress));
+		} else {
+			baseConfig.setLocalInetAddress(InetAddress.getLocalHost());
+		}
+
+		if (!isEmpty(sipPublicInetAddress)) {
+			baseConfig.setPublicInetAddress(InetAddress
+					.getByName(sipPublicInetAddress));
+		}
+
+		if (!isEmpty(sipPlaybackFile)) {
+			baseConfig.setMediaMode(MediaMode.file);
+			baseConfig.setMediaFile("./playback.wav");
+		} else {
+			baseConfig.setMediaMode(MediaMode.none);
+		}
+		return baseConfig;
+	}
+
+	private static boolean isEmpty(String str) {
+		return str == null || str.trim().isEmpty();
 	}
 }
